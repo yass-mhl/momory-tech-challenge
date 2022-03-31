9065a8ef4281c1d1820f6551bc6e91f804ea45f1
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-03-31 15:48:25 +0200

    Add Graph

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,8 @@ class PagesController < ApplicationController
     @nb_customers = nb_customers(country_select)
     @total_revenue = total_revenue(country_select).fdiv(1_000).round(0)
     @average_revenue = (@total_revenue * 1_000).fdiv(nb_orders(country_select)).round(1)
+    @months = graph(country_select)[0]
+    @revenues_per_month = graph(country_select)[1]
   end
 
   private
@@ -56,6 +58,30 @@ class PagesController < ApplicationController
       OrderProduct.all.each { |product| total_revenue += product.quantity * product.unit_price }
     end
     return total_revenue
+  end
+
+  def graph(country)
+    orders = []
+    if country
+      Customer.where(country: country).each { |customer| orders += customer.orders }
+    else
+      orders = Order.all
+    end
+
+    last_order = orders.max_by { |order| order.date }
+    last_order_month = last_order.date.month
+
+    months = []
+    revenues = []
+
+    (0..11).each do |i|
+      month = (last_order_month + i) % 12 + 1
+      month_orders = orders.select { |order| order.date.month == month }
+      revenues << month_orders.reduce(0) { |sum, order| sum + order.total_price.fdiv(1_000).round(0) }
+      months << Date.new(2020, month, 1).strftime("%B")
+    end
+
+    return [months, revenues]
   end
 
   def search_params
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,7 @@
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
+import Chart from 'chart.js/auto';
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
@@ -14,3 +15,66 @@ ActiveStorage.start()
 
 import "controllers"
 import "bootstrap"
+
+console.log('Chart', Chart)
+
+document.addEventListener("turbolinks:load", function() {
+  loadChart();
+});
+
+const loadChart = () => {
+
+  var xData = document.getElementById('chart-x-data').dataset;
+  var yData = document.getElementById('chart-y-data').dataset;
+
+  var ctx = document.getElementById('myChart');
+  var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: [xData.m1, xData.m2, xData.m3, xData.m4, xData.m5, xData.m6, xData.m7, xData.m8, xData.m9, xData.m10, xData.m11, xData.m12],
+          datasets: [{
+              label: 'Revenue in k€',
+              data: [yData.m1, yData.m2, yData.m3, yData.m4, yData.m5, yData.m6, yData.m7, yData.m8, yData.m9, yData.m10, yData.m11, yData.m12],
+              backgroundColor: [
+                  'rgba(237, 137, 137, 0.2)',
+                  'rgba(237, 137, 137, 0.2)',
+                  'rgba(237, 137, 137, 0.2)',
+                  'rgba(237, 137, 137, 0.2)',
+                  'rgba(237, 137, 137, 0.2)',
+                  'rgba(237, 137, 137, 0.2)',
+                  'rgba(237, 137, 137, 0.2)',
+                  'rgba(237, 137, 137, 0.2)',
+                  'rgba(237, 137, 137, 0.2)',
+                  'rgba(237, 137, 137, 0.2)',
+                  'rgba(237, 137, 137, 0.2)',
+                  'rgba(237, 137, 137, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(237, 137, 137, 1))',
+                  'rgba(237, 137, 137, 1))',
+                  'rgba(237, 137, 137, 1))',
+                  'rgba(237, 137, 137, 1))',
+                  'rgba(237, 137, 137, 1))',
+                  'rgba(237, 137, 137, 1))',
+                  'rgba(237, 137, 137, 1))',
+                  'rgba(237, 137, 137, 1))',
+                  'rgba(237, 137, 137, 1))',
+                  'rgba(237, 137, 137, 1))',
+                  'rgba(237, 137, 137, 1))',
+                  'rgba(237, 137, 137, 1))'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+  });
+
+};
